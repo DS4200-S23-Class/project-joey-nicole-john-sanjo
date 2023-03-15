f9e8338fe25731fe69f8db45460a15b4ba44e67f
jdrohan356 <jdrohan356@gmail.com>
2023-03-15 15:22:14 -0400

    Make vis

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const FRAME_HEIGHT = 500;
+const FRAME_WIDTH = 500; 
+const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
+
+const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
+const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right; 
+
+const FRAME1 = d3.select("#vis1")
+                  .append("svg")
+                    .attr("height", FRAME_HEIGHT)
+                    .attr("width", FRAME_WIDTH)
+                    .attr('id', 'spsvg')
+                    .attr("class", "frame");
+
+
+function build_vis(){
+  
+    // reset graph if new points are being added 
+  document.getElementById("spsvg").innerHTML = '';
+  d3.csv("finished.csv").then((data) => {
+    
+    // get data from page
+    console.log(data)
+  });
+}
