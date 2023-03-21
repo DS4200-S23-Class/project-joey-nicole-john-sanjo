e7d22f665101a3d5ec5b95ed6f3b584291e57b92
jdrohan356 <jdrohan356@gmail.com>
2023-03-20 22:19:39 -0400

    made lines

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ function display_rows(){
   
     // reset graph if new points are being added 
   document.getElementById("spsvg").innerHTML = '';
-  d3.csv("finished_1.csv").then((data) => {
+  d3.csv("finished.csv").then((data) => {
 
     print_len = 10
 
@@ -30,7 +30,7 @@ function display_rows(){
 
 function build_scatter_plot(addpoints, newdata) {
 
-    d3.csv("finished.csv").then((data) => {
+    d3.csv("line.csv").then((data) => {
     
     const grouped = d3.group(data, d => d.rating); 
     console.log(grouped)
@@ -41,42 +41,36 @@ function build_scatter_plot(addpoints, newdata) {
     .domain(d3.extent(data, function(d) { return d.release_year; }))
     .range([ 0, VIS_WIDTH]);
      FRAME1.append("g")
-    .attr("transform", `translate(0, ${VIS_HEIGHT})`)
+     .attr("transform", "translate(" + MARGINS.left + 
+                  "," + (VIS_HEIGHT + MARGINS.top) + ")") 
     .call(d3.axisBottom(x_scale).ticks(5));
 
      // Add Y axis
      const y_scale = d3.scaleLinear()
-    .domain([0, 600]) // fix this
+    .domain([0, 200]) // fix this
     .range([VIS_HEIGHT, 0]);
-     FRAME1.append("g")
+     FRAME1.append("g").attr("transform", "translate(" + MARGINS.left + 
+     "," + (MARGINS.bottom) + ")") 
     .call(d3.axisLeft(y_scale));
       
+    // color palette
+    const color = d3.scaleOrdinal()
+    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
+
     // Draw the line
     FRAME1.selectAll(".line")
         .data(grouped)
         .join("path")
           .attr("fill", "none")
+          .attr("stroke", function(d){ return color(d[0]) })
           .attr("stroke-width", 1.5)
           .attr("d", function(d){
             return d3.line()
               .x(function(d) { return x_scale(d.release_year); })
-              .y(function(d) { return x_scale(d.n); })
-              (d[1])})
-  
-      // // Adds an X axis to the scatter plot
-      // FRAME1.append("g") 
-      //       .attr("transform", "translate(" + MARGINS.left + 
-      //             "," + (VIS_HEIGHT + MARGINS.top) + ")") 
-      //       .call(d3.axisBottom(X_SCALE1).ticks(10)) 
-      //         .attr("font-size", '20px'); 
-  
-      // // Adds a Y axis to the scatter plot
-      // FRAME1.append("g") 
-      //     .attr("transform", "translate(" + MARGINS.left + 
-      //         "," + (MARGINS.bottom) + ")") 
-      //     .call(d3.axisLeft(Y_SCALE1).ticks(10)) 
-      //         .attr("font-size", '20px'); 
-  
+              .y(function(d) { return y_scale(+d.Count); })
+              (d[1])
+          })
+
   
     });
   }
